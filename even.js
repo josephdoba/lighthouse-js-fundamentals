51a3133937dec610475e07e90c366f5d6e66e428
Joseph Doba <josephdoba@gmail.com>
2022-02-25 16:25:49 -0800

    More function exercises

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const numIsEven = isEven(6);
+
+
+console.log(tenIsEven);
+console.log(numIsEven);
--- hello.js
+++ hello.js
@@ -1,5 +1,15 @@
+/* Console.logging is different to Returning values from functions: 
+
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello("Joba");
+*/
+// Returning values:
+const returnSayHello  = function (name) {
+  return("Hello, " + name);
+}
+
+const greeting = returnSayHello('Joba');
+console.log(greeting);

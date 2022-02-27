403c1357c7cd7226d106d1396ca167fc39c57d41
Joseph Doba <josephdoba@gmail.com>
2022-02-26 18:29:04 -0800

    Added while loop over Array content

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,20 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+
+// You can use a For Loop, or a While Loop to iterate over arrays
+
+//For Loop:
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
 packingList[3]
+
+// While Loop: 
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
